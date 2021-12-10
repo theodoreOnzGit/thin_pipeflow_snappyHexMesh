f31c345f325db7f8fb0c243164866989feacb7ad
teddy0 @ aftershock manjaro big pc <fifadude114@gmail.com>
2021-12-09 23:45:31 -0800

    dimensions and internalField classes written

--- intermediatePlus/salome/videoScripts/inputOutput.py
+++ intermediatePlus/salome/videoScripts/inputOutput.py
@@ -4,6 +4,9 @@ class foamWriter:
 
         print("foamWriter object initiated")
 
+        self.setKinematicPressureInternalField(0)
+        self.setVelocityInternalField(0,0,0)
+
 
     def writeHeader(self,fileName):
 
@@ -30,15 +33,122 @@ class foamWriter:
 
         f.close()
 
+
+    def writeFoamFileSection(self,fileName,fieldType):
+
+        if fieldType == "vector":
+
+            fieldType = "volVectorField"
+
+        f = self.getAppendObj(fileName = fileName)
+
+        f.write(r"FoamFile")
+        f.write("\n")
+        f.write(r"{")
+        f.write("\n")
+        f.write(r"    version     2.0;")
+        f.write("\n")
+        f.write(r"    format      ascii;")
+        f.write("\n")
+        f.write(r"    class       "+fieldType+";")
+        f.write("\n")
+        f.write(r"    object      "+fileName+";")
+        f.write("\n")
+        f.write(r"}")
+        f.write("\n")
+
+        f.close()
+
+    def writeHeader2(self,fileName):
+
+        f = self.getAppendObj(fileName = fileName)
+        f.write("// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //")
+        f.write("\n")
+        f.write(" ")
+        f.write("\n")
+        f.close()
+
+    def writeKinematicPressureDimensions(self):
+
+        
+        f = self.getAppendObj(fileName = "p")
+
+        f.write("dimensions      [0 2 -2 0 0 0 0];")
+        f.write("\n")
+        f.write(" ")
+        f.write("\n")
+
+        f.close()
+
+
+    def writeVelocityDimensions(self):
+
+        
+        f = self.getAppendObj(fileName = "U")
+
+        f.write("dimensions      [0 1 -1 0 0 0 0];")
+        f.write("\n")
+        f.write(" ")
+        f.write("\n")
+
+        f.close()
+
+    def writeInternalField(self,fileName,internalFieldValue):
+
+        internalFieldValue = str(internalFieldValue)
+
+        f = self.getAppendObj(fileName = fileName)
+
+        f.write("internalField   uniform " + internalFieldValue +";")
+        f.write("\n")
+        f.write(" ")
+        f.write("\n")
+
+        f.close()
+
+
+    def writeVelocityInternalField(self):
+
+        velocityField = self.getVelocityInternalField()
+
+        ux = velocityField[0]
+        uy = velocityField[1]
+        uz = velocityField[2]
+
+        internalFieldValue = "("+str(ux)+" "+str(uy)+" "+str(uz)+")"
+
+        self.writeInternalField(fileName = "U", internalFieldValue = internalFieldValue)
+
+    def writeKinematicPressureInternalField(self):
+
+        self.writeInternalField(fileName = "p", internalFieldValue = self.getKinematicPressureInternalField())
+
     def writeVelocityFile(self):
 
         self.writeHeader(fileName = 'U')
+
+        self.writeFoamFileSection(fileName = 'U', fieldType = 'volVectorField')
+
+        self.writeHeader2(fileName = "U")
+
+        self.writeVelocityDimensions()
+
+        self.writeVelocityInternalField()
 
         print("velocity file written")
 
     def writeKinematicPressureFile(self):
 
         self.writeHeader(fileName = 'p')
+
+        self.writeFoamFileSection(fileName = 'p', fieldType = 'volScalarField')
+
+        self.writeHeader2(fileName = "p")
+
+        self.writeKinematicPressureDimensions()
+
+        self.writeKinematicPressureInternalField()
+
 
         print("pressure file written")
 
@@ -60,3 +170,26 @@ class foamWriter:
         f = self.getOpenObj(fileName = fileName, mode = "a")
 
         return f
+
+
+##### get and set functions are here #####
+
+
+    def setVelocityInternalField(self,ux,uy,uz):
+
+        self.ux = ux
+        self.uy = uy
+        self.uz = uz
+
+    def getVelocityInternalField(self):
+
+        return (self.ux,self.uy,self.uz)
+
+
+    def setKinematicPressureInternalField(self,kinematicPressure):
+
+        self.kinematicPressure = kinematicPressure
+
+    def getKinematicPressureInternalField(self):
+
+        return self.kinematicPressure
